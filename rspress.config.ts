4cb8582dd65047df52cdcab3bf792bff9ff45587
Jeongjin Oh <rojay.developer@gmail.com>
2025-01-28 16:27:45 +0900

    chore: remove node polyfill

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,42 +1,40 @@
-import * as path from 'node:path';
-import { defineConfig } from 'rspress/config';
-import { pluginNodePolyfill } from '@rsbuild/plugin-node-polyfill'
+import * as path from "node:path";
+import { defineConfig } from "rspress/config";
 
 export default defineConfig({
-  globalStyles: path.join(__dirname, 'src/styles/index.css'),
-  root: path.join(__dirname, 'docs'),
-  title: 'Jeongjin.me',
-  icon: '/rspress-icon.png',
+  globalStyles: path.join(__dirname, "src/styles/index.css"),
+  root: path.join(__dirname, "docs"),
+  title: "Jeongjin.me",
+  icon: "/rspress-icon.png",
   logo: {
-    light: '/rspress-light-logo.png',
-    dark: '/rspress-dark-logo.png',
+    light: "/rspress-light-logo.png",
+    dark: "/rspress-dark-logo.png",
   },
   builderConfig: {
-    plugins: [pluginNodePolyfill()],
     resolve: {
       alias: {
-        '@/components': './src/components'
-      }
-    }
+        "@/components": "./src/components",
+      },
+    },
   },
   themeConfig: {
     enableContentAnimation: true,
     socialLinks: [
       {
-        icon: 'github',
-        mode: 'link',
-        content: 'https://github.com/ojj1123/',
+        icon: "github",
+        mode: "link",
+        content: "https://github.com/ojj1123/",
       },
       {
-        icon: 'linkedin',
-        mode: 'link',
-        content: 'https://www.linkedin.com/in/jeongjin-oh/'
+        icon: "linkedin",
+        mode: "link",
+        content: "https://www.linkedin.com/in/jeongjin-oh/",
       },
       {
-        icon: 'bluesky',
-        mode: 'link',
-        content: 'https://bsky.app/profile/jeongjin.me'
-      }
+        icon: "bluesky",
+        mode: "link",
+        content: "https://bsky.app/profile/jeongjin.me",
+      },
     ],
   },
 });
