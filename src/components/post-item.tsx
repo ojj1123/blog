6fad2e15aae79bbf0ec218a3dce2d8a10eb33e83
Jeongjin Oh <rojay.developer@gmail.com>
2025-01-29 22:43:20 +0900

    feat: get post data using usePageData hook

--- src/components/post-item.tsx
+++ src/components/post-item.tsx
@@ -1,35 +1,36 @@
+import type { BaseRuntimePageInfo } from "@rspress/shared";
+import { Link } from "rspress/theme";
+
 interface PostItemProps {
-  post: {
-    index: number;
-    image: string;
-    title: string;
-    date: string;
-  };
+  isFirst: boolean;
+  post: BaseRuntimePageInfo;
 }
 
-export default function PostItem({ post: { index } }: PostItemProps) {
+export default function PostItem({ post, isFirst }: PostItemProps) {
   return (
     <li
-      className={`flex flex-col overflow-hidden rounded-sm transition-shadow hover:shadow-lg ${index === 0 ? "lg:col-span-2 lg:row-span-2" : ""}`}
+      className={`flex flex-col overflow-hidden rounded-sm transition-shadow hover:shadow-lg ${isFirst ? "lg:col-span-2 lg:row-span-2" : ""}`}
     >
-      <div className="relative w-full flex-1">
-        <img src="/og-image.png" alt="og-image" className="object-cover" />
-      </div>
-
-      <div className="p-4">
-        <h2 className="hover:text-primary line-clamp-2 text-lg font-semibold transition-colors">
-          title title
-        </h2>
-        <div className="flex items-center text-sm">
-          <time dateTime={new Date().toISOString()}>
-            {new Date(new Date().toISOString()).toLocaleDateString("en-US", {
-              year: "numeric",
-              month: "long",
-              day: "numeric",
-            })}
-          </time>
+      <Link href={post.routePath}>
+        <div className="relative w-full flex-1">
+          <img src="/og-image.png" alt="og-image" className="object-cover" />
         </div>
-      </div>
+
+        <div className="p-4">
+          <h2 className="hover:text-primary line-clamp-2 text-lg font-semibold transition-colors">
+            {post.title}
+          </h2>
+          <div className="flex items-center text-sm">
+            <time dateTime={new Date().toISOString()}>
+              {new Date(new Date().toISOString()).toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              })}
+            </time>
+          </div>
+        </div>
+      </Link>
     </li>
   );
 }
--- src/components/post-list.tsx
+++ src/components/post-list.tsx
@@ -1,31 +1,14 @@
-import * as fs from "node:fs";
 import PostItem from "./post-item";
-
-const postsFolder = "/docs";
-
-// FIXME fs 모듈로 파일 이름 읽어오기
-// const posts = fs.readdirSync(postsFolder);
-const posts = [
-  { pathname: "post1" },
-  { pathname: "post2" },
-  { pathname: "post3" },
-  { pathname: "post4" },
-  { pathname: "post1" },
-  { pathname: "post2" },
-  { pathname: "post3" },
-  { pathname: "post4" },
-];
+import { usePageData } from "rspress/runtime";
 
 export default function PostList() {
+  const { siteData } = usePageData();
+  const posts = siteData.pages.filter((page) => page.routePath.startsWith("/posts"));
+
   return (
     <ul className="grid gap-6 px-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
       {posts.map((post, index) => {
-        return (
-          <PostItem
-            key={index}
-            post={{ index, image: "", title: "title", date: new Date().toISOString() }}
-          />
-        );
+        return <PostItem key={post.routePath} isFirst={index === 0} post={post} />;
       })}
     </ul>
   );
